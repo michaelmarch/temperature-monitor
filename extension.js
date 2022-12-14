e90ad4a092c866bffb6fb51643895073b2246151
michaelmarch <s222074@student.uek.krakow.pl>
2022-12-14 19:51:33 +0100

    Fix CPU temperature detection. Refactor code. Add comments.

--- extension.js
+++ extension.js
@@ -4,20 +4,19 @@ const Me = ExtensionUtils.getCurrentExtension();
 
 const Main = imports.ui.main;
 
+const {
+    getGPUTemperatureCommand,
+    getCPUTemperatureCommand,
+    updateAll,
+    updateCPU,
+    logError,
+} = Me.imports.utils;
+
 const { St, GLib } = imports.gi;
-const { detectGPU, updateCPU, updateAll } = Me.imports.utils;
-const { GPU_NONE, GPU_AMD, GPU_NVIDIA, CPU } = Me.imports.constants;
 
-const Commands = {
-    [GPU_NONE]: "",
-    [GPU_AMD]: "",
-    [GPU_NVIDIA]: ["nvidia-settings", "-q", "gpucoretemp", "-t"],
-    [CPU]: "/sys/class/thermal/thermal_zone0/temp",
-};
+let gpuPanel, cpuPanel, gpuPanelText, cpuPanelText;
 
-let gpuPanel, cpuPanel, gpuPanelText, cpuPanelText, GPU;
-
-function init() {
+var init = () => {
     gpuPanel = new St.Bin({
         style_class: "panel-button",
     });
@@ -33,33 +32,49 @@ function init() {
         style_class: "label",
     });
     cpuPanel.set_child(cpuPanelText);
-}
+};
 
-async function enable() {
-    GPU = await detectGPU(); // TODO: test the view without gpu monitor
-    Main.panel._rightBox.insert_child_at_index(gpuPanel, 0);
-    Main.panel._rightBox.insert_child_at_index(cpuPanel, 0);
+var enable = async () => {
+    const priority = GLib.PRIORITY_LOW;
+    const updateInterval = 2.0;
 
     try {
-        GLib.timeout_add_seconds(
-            0,
-            1.0,
-            GPU === GPU_NONE
-                ? updateCPU.bind(this, Commands[CPU], cpuPanelText)
-                : updateAll.bind(
-                      this,
-                      Commands[CPU],
-                      cpuPanelText,
-                      Commands[GPU],
-                      gpuPanelText
-                  )
-        );
+        let gpuCommand = await getGPUTemperatureCommand();
+        let cpuCommand = await getCPUTemperatureCommand();
+
+        if (gpuCommand && cpuCommand) {
+            GLib.timeout_add_seconds(
+                priority,
+                updateInterval,
+                updateAll.bind(
+                    this,
+                    cpuCommand,
+                    cpuPanelText,
+                    gpuCommand,
+                    gpuPanelText
+                )
+            );
+        } else if (gpuCommand) {
+            GLib.timeout_add_seconds(
+                priority,
+                updateInterval,
+                updateGPU.bind(this, gpuCommand, gpuPanelText)
+            );
+        } else if (cpuCommand) {
+            GLib.timeout_add_seconds(
+                priority,
+                updateInterval,
+                updateCPU.bind(this, cpuCommand, cpuPanelText)
+            );
+        }
+        Main.panel._rightBox.insert_child_at_index(gpuPanel, 0);
+        Main.panel._rightBox.insert_child_at_index(cpuPanel, 0);
     } catch (e) {
         logError(e);
     }
-}
+};
 
-function disable() {
+var disable = () => {
     Main.panel._rightBox.remove_child(gpuPanel);
     Main.panel._rightBox.remove_child(cpuPanel);
-}
+};
--- utils.js
+++ utils.js
@@ -1,15 +1,21 @@
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
-const { GPU_NONE, GPU_AMD, GPU_NVIDIA } = Me.imports.constants;
 const { Gio, GLib } = imports.gi;
 
-const decoder = new TextDecoder("ascii");
+//* Note: Only functions declared with 'var' keyword are automatically exported
 
-var detectGPU = async () => {
+/**
+ *  Finds the command for fetching temperature of a GPU.
+ ** Note: In a case of multiple GPUs installed in the system the first GPU listed by `lspci` will be used.
+ *
+ *  @returns {string[] | undefined} The command split with ' ' (white space), which an be directly passed to {@link execCommunicate}
+ *                                  or `undefined` if GPU couldn't be detected.
+ */
+var getGPUTemperatureCommand = async () => {
     try {
-        let output = await execCommunicate(["lspci", "-nnk"]);
-        let lines = output.split("\n");
+        const output = await execCommunicate(["lspci", "-nnk"]);
+        const lines = output.split("\n");
 
         for (let line of lines) {
             line = line.toLowerCase();
@@ -19,54 +25,120 @@ var detectGPU = async () => {
             }
 
             if (line.includes("nvidia")) {
-                return GPU_NVIDIA;
+                return ["nvidia-settings", "-q", "gpucoretemp", "-t"];
             } else if (line.includes("amd") || line.includes("radeon")) {
-                return GPU_AMD;
+                //return [""];
             }
         }
     } catch (e) {
         logError(e);
     }
-
-    return GPU_NONE;
 };
 
-var updateAll = async (cpuCommand, cpuLabel, gpuCommand, gpuLabel) => {
+/**
+ * Finds the command for fetching temperature of a CPU.
+ *
+ * @returns {string[] | undefined} The command split with ' ' (white space), which an be directly passed to {@link execCommunicate}
+ *                                 or `undefined` if GPU couldn't be detected.
+ */
+var getCPUTemperatureCommand = async () => {
     try {
-        let output = await execCommunicate(gpuCommand);
-        gpuLabel.set_text(`G: ${output} °C`);
+        let thermalZones = (await execCommunicate(["ls", "/sys/class/thermal"]))
+            .split("\n")
+            .filter((e) => e.includes("thermal_zone"));
 
-        let [, output2] = await readFile(cpuCommand);
-        output2 = parseInt(decoder.decode(output2) / 1000);
-        cpuLabel.set_text(`C: ${output2} °C`);
+        let pkgThermalZone = await findAsync(
+            thermalZones,
+            async (thermalZone) => {
+                let thermalZoneType = await execCommunicate([
+                    "cat",
+                    `/sys/class/thermal/${thermalZone}/type`,
+                ]);
+                return thermalZoneType === "x86_pkg_temp";
+            }
+        );
+
+        return ["cat", `/sys/class/thermal/${pkgThermalZone}/temp`];
     } catch (e) {
         logError(e);
-        return GLib.SOURCE_REMOVE;
     }
-   
-    return GLib.SOURCE_CONTINUE;
 };
 
+/**
+ * Callback function for updating all monitors.
+ * 
+ * @param {string[]} cpuCommand - A command for fetching the CPU temperature obtained from {@link getCPUTemperatureCommand}
+ * @param {St.Label} cpuLabel 
+ * @param {string[]} gpuCommand - A command for fetching the GPU temperature obtained from {@link getGPUTemperatureCommand}
+ * @param {St.Label} gpuLabel 
+ * 
+ * @returns {boolean} Either `GLib.SOURCE_REMOVE` or `GLib.SOURCE_CONTINUE`
+ */
+var updateAll = async (cpuCommand, cpuLabel, gpuCommand, gpuLabel) => {
+    return (
+        (await updateCPU(cpuCommand, cpuLabel)) &&
+        (await updateGPU(gpuCommand, gpuLabel))
+    );
+};
+
+/**
+ * Callback function for updating CPU monitors.
+ * 
+ * @param {string[]} command - A command for fetching the CPU temperature obtained from {@link getCPUTemperatureCommand}
+ * @param {St.Label} label 
+ * 
+ * @returns {boolean} Either `GLib.SOURCE_REMOVE` or `GLib.SOURCE_CONTINUE`
+ */
 var updateCPU = async (command, label) => {
     try {
-        let [, output] = await readFile(command);
-        output = parseInt(decoder.decode(output) / 1000);
+        let output = (await execCommunicate(command)) / 1000;
         label.set_text(`C: ${output} °C`);
     } catch (e) {
         logError(e);
         return GLib.SOURCE_REMOVE;
     }
-   
+
     return GLib.SOURCE_CONTINUE;
 };
 
-var logError = (e) => {
-    log("***************** CAUGHT EXCEPTION *****************");
-    global.logError(e);
-    log(`${e.stack}`);
+/**
+ * Callback function for updating GPU monitors.
+ * 
+ * @param {string[]} command - A command for fetching the GPU temperature obtained from {@link getGPUTemperatureCommand}
+ * @param {St.Label} label 
+ * 
+ * @returns {boolean} Either `GLib.SOURCE_REMOVE` or `GLib.SOURCE_CONTINUE`
+ */
+var updateGPU = async (command, label) => {
+    try {
+        let output = (await execCommunicate(command));
+        label.set_text(`G: ${output} °C`);
+    } catch (e) {
+        logError(e);
+        return GLib.SOURCE_REMOVE;
+    }
+
+    return GLib.SOURCE_CONTINUE;
 };
 
-var execCommunicate = (argv) => {
+/**
+ * Logs the error. To check for errors use: `journalctl -f -o cat /usr/bin/gnome-shell`
+ * 
+ * @param {Error} error
+ */
+var logError = (error) => {
+    log("***************** CAUGHT EXCEPTION *****************");
+    global.logError(error);
+    log(error.stack);
+};
+
+/**
+ * 
+ * @param {string[]} argv A command split by ' ' (white space)
+ * 
+ * @returns {Promise<string>} - Standard output of the supplied command if successful
+ */
+let execCommunicate = (argv) => {
     try {
         let flags =
             Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE;
@@ -78,17 +150,15 @@ var execCommunicate = (argv) => {
                 try {
                     let [, stdout, stderr] = proc.communicate_utf8_finish(res);
 
-                    if (!proc.get_successful()) {
-                        let status = proc.get_exit_status();
-                        throw new Gio.IOErrorEnum({
-                            code: Gio.io_error_from_errno(status),
-                            message: stderr
-                                ? stderr.trim()
-                                : GLib.strerror(status),
-                        });
+                    if (proc.get_successful()) {
+                        resolve(stdout.trim());
                     }
 
-                    resolve(stdout.trim());
+                    let status = proc.get_exit_status();
+                    throw new Gio.IOErrorEnum({
+                        code: Gio.io_error_from_errno(status),
+                        message: stderr ? stderr.trim() : GLib.strerror(status),
+                    });
                 } catch (e) {
                     reject(e);
                 }
@@ -99,16 +169,18 @@ var execCommunicate = (argv) => {
     }
 };
 
-var readFile = (path) => {
-    const file = Gio.File.new_for_path(path);
-
-    return new Promise((resolve, reject) => {
-        file.load_contents_async(null, (_file, result) => {
-            try {
-                resolve(file.load_contents_finish(result));
-            } catch (e) {
-                reject(e);
-            }
-        });
-    });
+/**
+ * Asynchronous implementation of {@link Array.find}: https://stackoverflow.com/a/55601090
+ * 
+ * @template T
+ * @param {T[]} array 
+ * @param {(e: T) => boolean} asyncCallback 
+ * 
+ * @returns {Promise<T | undefined>} 
+ */
+let findAsync = async (array, asyncCallback) => {
+    const promises = array.map(asyncCallback);
+    const results = await Promise.all(promises);
+    const index = results.findIndex((result) => result);
+    return array[index];
 };
